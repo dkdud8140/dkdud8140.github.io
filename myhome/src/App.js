d138d4d8cdb6ae3925f90dc6e1695a3c5f11b9d2
dkdud8140 <cho.ay4@gmail.com>
2021-11-23 21:22:36 +0900

    CreateMyHome

---
+++ myhome/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>조아영 포트폴리오</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
